52279d41099afcbeeb0e52426dba17d01f20be1a
Umberto <umbertopellegrino96@gmail.com>
2020-09-04 16:34:48 +0200

    PrintSerch()

--- vers2/script.js
+++ vers2/script.js
@@ -1,3 +1,37 @@
+function serch() {
+  var bt = $('#bt');
+
+  bt.click(function () {
+    var input = $('.serch #input').val();
+    $('#albums').text('');
+    $('.serch #input').val('');
+
+    $.ajax({
+      url:'index.php',
+      method:'GET',
+      success:function (data) {
+        printserch(data['response'],input);
+      },
+      error:function (err) {
+        console.log(err);
+      }
+    });
+  });
+}
+
+function printserch(data,input) {
+  var target = $('#albums');
+  var template = $('#album-template').html();
+  var compiled = Handlebars.compile(template);
+
+  for (var i = 0; i < data.length; i++) {
+    var album =data[i];
+    if (album['author'] == input) {
+      target.append(compiled(album));
+    }
+  };
+}
+
 function getData() {
   $.ajax({
     url:'index.php',
@@ -25,6 +59,7 @@ function printPoster(data) {
 
 function init() {
   getData();
+  serch();
 }
 
 $(document).ready(init);
